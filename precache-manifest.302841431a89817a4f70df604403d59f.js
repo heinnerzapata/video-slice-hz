03e6531127c3696fdcbbfcef44d44f5469143fd7
Heinner Zapata <heinnerzapata1991@gmail.com>
2018-12-19 23:38:53 -0500

    Updates

---
+++ precache-manifest.302841431a89817a4f70df604403d59f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f4a95550030459d16ac8",
+    "url": "/static/js/main.f4a95550.chunk.js"
+  },
+  {
+    "revision": "785b529afa26b9769b91",
+    "url": "/static/js/1.785b529a.chunk.js"
+  },
+  {
+    "revision": "f4a95550030459d16ac8",
+    "url": "/static/css/main.380f59c6.chunk.css"
+  },
+  {
+    "revision": "785b529afa26b9769b91",
+    "url": "/static/css/1.05d5530a.chunk.css"
+  },
+  {
+    "revision": "a63bd4074a03c45110807194e086cfcd",
+    "url": "/index.html"
+  }
+];
